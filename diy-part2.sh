83e78689dbd9a599311613ff2df196865a78ece3
Ray <ray@jover.com>
2025-07-23 17:01:30 +0800

    temp fix

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' target/linux/mediatek/Makefile
+
 sed -i '/$(PKG_BUILD_DIR)\/firmware\/mt7981_wm\.bin \\/a \\t$(PKG_BUILD_DIR)\/firmware\/mt7981_wo\.bin \\' package/kernel/mt76/Makefile
 wget https://downloads.openwrt.org/snapshots/targets/mediatek/filogic/openwrt-toolchain-mediatek-filogic_gcc-14.3.0_musl.Linux-x86_64.tar.zst
 tar xvf openwrt-toolchain-mediatek-filogic_gcc-14.3.0_musl.Linux-x86_64.tar.zst
