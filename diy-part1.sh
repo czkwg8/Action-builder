9eb3d144a39dc8215f94bae2b10ce189c4971503
Ray <ray@jover.com>
2025-07-25 12:52:36 +0800

    new config

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,5 +2,3 @@
 
 # Add a feed source
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
-git config --global user.name "Joker"
-git revert --no-edit 5359639c2b743be800ebe075936946c48635333a
