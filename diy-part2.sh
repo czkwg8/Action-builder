2987adf354b0d4e29e315e039f6498bfdb52dac4
R.C. <czkwg8@gmail.com>
2025-08-19 19:20:01 +0800

    new conf and remove unnecessary firmware mod

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 
-sed -i '/$(PKG_BUILD_DIR)\/firmware\/mt7981_wm\.bin \\/a \\t$(PKG_BUILD_DIR)\/firmware\/mt7981_wo\.bin \\' package/kernel/mt76/Makefile
 wget https://downloads.openwrt.org/snapshots/targets/mediatek/filogic/openwrt-toolchain-mediatek-filogic_gcc-14.3.0_musl.Linux-x86_64.tar.zst
 tar xvf openwrt-toolchain-mediatek-filogic_gcc-14.3.0_musl.Linux-x86_64.tar.zst
 wget https://downloads.openwrt.org/snapshots/targets/mediatek/filogic/llvm-bpf-20.1.8.Linux-x86_64.tar.zst
